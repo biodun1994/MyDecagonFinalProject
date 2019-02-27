1ad22c86ba85d326d40f68e35249edfce322eb84
biodun1994 <olapadeabiodun21@gmail.com>
2019-02-27 17:43:48 +0100

    Login feature fully Implemented

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 //the very beginning of jQuery initiation 
 $(document).ready(() =>{
+
+    //sign-up implementation begins here...
     $("#signup-form").on("submit", (e) =>{
 
         //prevents default behaviour
@@ -7,7 +9,7 @@ $(document).ready(() =>{
 
         //input validations
         if($("#reg-email").val() === ''){
-            console.log("email is required");
+            // console.log("email is required");
             Swal.fire({
                 title: 'Caution!',
                 text: 'Email is required!',
@@ -24,8 +26,8 @@ $(document).ready(() =>{
                 timer:3000
               });
         }else if($("#reg-password").val() !== $("#cfm-password").val()){
-            console.log($("#reg-password").val(), $("#cfm-password").val());
-            console.log("passwords do not match");
+            // console.log($("#reg-password").val(), $("#cfm-password").val());
+            // console.log("passwords do not match");
             Swal.fire({
                 title: 'Caution!',
                 text: "Password Fields didn't match!",
@@ -51,7 +53,7 @@ $(document).ready(() =>{
             encode: true
         })
         .done(() =>{
-            console.log("Data posted successfully");
+            // console.log("Data posted successfully");
             Swal.fire({
                 title: 'Successful!',
                 text: 'You can procede to login',
@@ -61,7 +63,7 @@ $(document).ready(() =>{
               })
         })
         .fail(() =>{
-            console.log("Error postion data");
+            // console.log("Error postion data");
             Swal.fire({
                 title: 'Error!',
                 text: 'Sorry, an error occurred',
@@ -74,5 +76,62 @@ $(document).ready(() =>{
         $("#signup-form").trigger("reset");
         }
     });
+    //sign-up implementation ends here...
+
+    //login-up implementation begins here...
+    $("#login-form").on("submit", (e) =>{
+
+        // prevent deafult behaviour
+        e.preventDefault();
+
+        //input validations
+        if($("#email").val() === "" || $("#password").val() === ""){
+            // console.log("button working");
+            Swal.fire({
+                title: 'Caution!',
+                text: 'All felds is required!',
+                type: 'error',
+                confirmButtonText: 'Ok',
+                timer:3000
+              });
+        }else{
+            const user = {
+                email:$("#email").val(),
+                password: $("#password").val()
+            }
+            $.getJSON("http://localhost:3000/users", user)
+            .done( (data) =>{
+                // console.log(data);
+                if(data.length === 0){
+                    Swal.fire({
+                        title: 'Error!',
+                        text: 'wrong password or email',
+                        type: 'error',
+                        confirmButtonText: 'Try Again',
+                        timer:3000
+                      });
+                }else{
+                    Swal.fire({
+                        title: 'Login Successful!',
+                        type: 'success',
+                        confirmButtonText: 'Ok',
+                        timer:3000
+                      });  
+
+                      //take user to blog page
+                      window.location.href="index.html";
+                }
+            })
+            .fail(()=>{
+                Swal.fire({
+                    title: 'Error making request!',
+                    type: 'error',
+                    confirmButtonText: 'Try Again',
+                    timer:3000
+                  });
+            });
+        }
+    });
+
 
 });
